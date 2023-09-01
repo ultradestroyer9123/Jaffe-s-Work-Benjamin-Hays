c1304a93173ee613aa09509e85be9adde916de92
Benjamin Phillip Hays <67854569+ultradestroyer9123@users.noreply.github.com>
2023-09-01 11:38:55 -0700

    Finished Quiz.java
    Awaiting another project!

--- Jaffe-s-Work-Benjamin-Hays/Jaffe-s-Work-Benjamin-Hays/Hello_World_Example/Quiz.java
+++ Jaffe-s-Work-Benjamin-Hays/Jaffe-s-Work-Benjamin-Hays/Hello_World_Example/Quiz.java
@@ -5,29 +5,56 @@ public class Quiz {
   public static void main(String[] args) {
     Random rand = new Random();
     int int_random_1 = rand.nextInt(101); 
-    int int_random_2 = rand.nextInt(101);
-    int operater_selector = rand.nextInt(1);
+    int int_random_2 = rand.nextInt(int_random_1);
+    int operater_selector = rand.nextInt(2);
+    int tries = 3;
     String operator;
     String feedback;
-    
+    String feedback2;
+    String Question = null;
+    int answer;
     if (operater_selector == 1) {
         operator = "+";
+        answer = int_random_1 + int_random_2;
     } else {
         operator = "-";
+        answer = int_random_1 - int_random_2;
     }
-    int answer = 333;
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    // Create a Scanner object
     System.out.println("What is " + int_random_1 + operator + int_random_2);
-
-    String Question = myObj.nextLine();  // Read user input
     String convertAnswerToString = Integer.toString(answer);
-    
-    if (convertAnswerToString == Question) {
-        feedback = "Correct!";
-    } else {
-        feedback = "Incorrect.";
+    while (!convertAnswerToString.equals(Question)) {
+        try {
+            Scanner myObj = new Scanner(System.in);
+            Question = myObj.nextLine();  // Read user input
+            int Question_error_handle = Integer.parseInt(Question);
+            if (convertAnswerToString.equals(Question)) {
+                break;
+            }
+            else {
+                tries -= 1;
+                if (tries == 0) {
+                    System.out.println("You have no more tries left! The answer was " + answer);
+                    break;
+                }
+                System.out.println("That's not correct! You have " + tries + " left");
+                System.out.println("What is " + int_random_1 + operator + int_random_2);
+                
+            }
+        }
+        catch(Exception e) {
+            System.out.println("That is not an integer!");
+            System.out.println("What is " + int_random_1 + operator + int_random_2);
+        }
     }
-    System.out.println("That is " + feedback + " The answer is " + convertAnswerToString + ", not " + Question);  // Output user input
+        
+
+    
+    
+    if (convertAnswerToString.equals(Question)) {
+        System.out.println("That's correct! The answer is " + convertAnswerToString);
+    }
+    
   }
 }
 
