293b4f0b831b60216fe150be59575ab44746bb54
Benjamin Phillip Hays <67854569+ultradestroyer9123@users.noreply.github.com>
2023-09-12 11:16:51 -0700

    Fixed Arrays.java

--- Projects/Arrays.java
+++ Projects/Arrays.java
@@ -10,17 +10,21 @@ public class Arrays {
         // arm_muscles[0] outputs : Bicepts
         arm_muscles[0] = "Biceps";
         int total_muscles = arm_muscles.length;
-        System.out.println("Total Muscles: " + total_muscles + " | Iterating through each muscle");
+        System.out.println("Total Muscles: " + total_muscles + " | Iterating through each muscle\n");
         for (int iterate = 0; iterate < total_muscles; iterate++) {
             System.out.println(arm_muscles[iterate]);
         }
         
+        String seperator = ", ";
+        String line_of_muscles = String.join(seperator, arm_muscles);
+        
         // Another way to iterate
-        String line_of_muscles = "";
-        for (String muscle : arm_muscles) {
-            line_of_muscles += muscle + ", ";
-        }
-        line_of_muscles = line_of_muscles.substring(line_of_muscles.length()-2);
-        System.out.println(line_of_muscles);
+        // for (String muscle : arm_muscles) {
+        //     line_of_muscles += muscle + ", ";
+        // }
+        // line_of_muscles = line_of_muscles.substring(0, line_of_muscles.length()-2);
+        
+        System.out.println("\nUsing the join function on the array:");
+        System.out.println(line_of_muscles + "\n");
     }
 }
