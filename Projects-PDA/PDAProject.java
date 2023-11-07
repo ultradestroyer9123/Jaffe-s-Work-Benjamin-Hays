897d784f6cde022605d9a5f5fc06117a8fb6f877
Benjamin Phillip Hays <67854569+ultradestroyer9123@users.noreply.github.com>
2023-11-07 11:27:12 -0800

    Add files via upload

--- Projects-PDA/PDAProject.java
+++ Projects-PDA/PDAProject.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 // lower bound = /2 and add 7 upper = sub 7 and mult by 2
 public class PDAProject {
-    public boolean PermDatingAge(int age1,int age2) {
+    public static boolean PermDatingAge(int age1,int age2) {
         float upperbound = 0f;
         float lowerbound = 0f;
         if (age1 > age2) {
@@ -19,30 +19,34 @@ public class PDAProject {
         } else {
             return true;
         }
-        if (age1 > lowerbound and age2) {
-            // continue here
+        if (age1 > lowerbound && age1 < upperbound && age2 > lowerbound && age2 < upperbound) {
+            System.out.println("Age1: " + age1 + "Age2: " + age2 + "Upper: " + upperbound + "Lower: " + lowerbound);
         }
+        System.out.println("Age1: " + age1 + "Age2: " + age2 + "Upper: " + upperbound + "Lower: " + lowerbound);
+        return true;
     }
     
     public static void main (String[] args) {
         // This is where we ask for the users age to make sure they are capable of 'PDA'
-        int age = 0;
+        int age1 = 0;
+        int age2 = 0;
         int minimum_age = 0;
         while (true) {
-            System.out.println("How old are you?");
+            System.out.println("Write 2 ages seperated by a ', ':");
             Scanner scanner = new Scanner(System.in);
+            scanner.useDelimiter(", |\n");
             try {
-                age = scanner.nextInt();
-                System.out.println("You are " + age + " year(s) old");
+                age1 = scanner.nextInt();
+                age2 = scanner.nextInt();
                 break;
             } catch (InputMismatchException e) {
                 System.out.println("'" + scanner.next() + "' is not a number! Try again.");
             }
         }
-        if (age < minimum_age) {
-            System.out.println(age + " is too young unfortunately.");            
+        if (age1 < minimum_age || age2 < minimum_age) {
+            System.out.println(age1 + " is too young unfortunately.");            
         } else {
-            
+            PermDatingAge(age1,age2);
         }
     }
 }
