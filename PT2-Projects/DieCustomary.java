ef93db4e5f33a58687c16ce24960f07f7c24636f
Benjamin Phillip Hays <67854569+ultradestroyer9123@users.noreply.github.com>
2023-11-27 12:16:05 -0800

    PT2

---
+++ PT2-Projects/DieCustomary.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class DieCustomary
+{
+    private int value;
+    private int sides;
+    
+    public int DieCustomary() {
+        this.value = new Random().nextInt(this.sides) + 1;
+        return this.value;
+    }
+    
+    public void setSides(int newValue) {
+        this.sides = newValue;
+    }
+    
+    public void roll() {
+        DieCustomary number = new DieCustomary();
+        
+    }
+}
