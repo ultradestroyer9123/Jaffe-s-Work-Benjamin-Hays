5ae1e0aa1079dc14dfbe84fb9ae8a7701911156f
Benjamin Phillip Hays <67854569+ultradestroyer9123@users.noreply.github.com>
2023-12-01 11:44:49 -0800

    Finished DieN

--- PT2-Projects/DieN.java
+++ PT2-Projects/DieN.java
@@ -5,36 +5,21 @@ public class DieN
     private int value;
     private int sides;
     
-    public void main() {
-        this.value = new Random().nextInt(this.sides) + 1;
-    }
-    
-    public void setSides(int newValue) {
+    public DieN(int newValue) {
         this.sides = newValue;
     }
-    
-    public int roll20() {
-        int original = this.value;
-        setSides(20);
-        main();
-        setSides(original);
+    public void roll() {
+        this.value = new Random().nextInt(this.sides) + 1;
+    }
+    public int getValue() {
         return this.value;
     }
-    
-    public int roll6() {
-        int original = this.value;
-        setSides(6);
-        main();
-        setSides(original);
-        return this.value;
-    }
-    
-    public int rollN() {
-        main();
-        return this.value;
-    }
-    
-    public String getCurrentData() {
-        return "Current selected sides: " + Integer.toString(this.sides) + ", " + "Last Roll: " + Integer.toString(this.value);
+    public int rollAndGetValue() {
+        roll();
+        return getValue();
     }
 }
+
+
+
+
