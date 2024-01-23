275d0527534156e01156bcef527cda2fef998729
Benjamin Phillip Hays <67854569+ultradestroyer9123@users.noreply.github.com>
2024-01-23 11:44:45 -0800

    Add files via upload

--- Projects-Final/RayCastJava/Raycast/DrawFrame.java
+++ Projects-Final/RayCastJava/Raycast/DrawFrame.java
@@ -1,12 +1,21 @@
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
-
+import javax.swing.border.Border;
+import java.util.Random;
+import static java.lang.System.out;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Font;
+import java.net.CookieHandler;
 public class DrawFrame {
+    int pixel_size = 10;
+    int pixel_amount_x = 100;
+    int pixel_amount_y = 80;
     public DrawFrame(String grid, String facing) {
-        JFrame myJFrame = new JFrame();
+        JFrame frame = new JFrame();
     
-        myJFrame.addKeyListener(new KeyAdapter() {
+        frame.addKeyListener(new KeyAdapter() {
           public void keyPressed(KeyEvent e) {
             int keyCode = e.getKeyCode();
             if (keyCode == KeyEvent.VK_W) {
@@ -20,7 +29,18 @@ public class DrawFrame {
             }
           }
         });
-    
-        myJFrame.setVisible(true);
+        Font f1 = new Font(Font.SANS_SERIF, Font.PLAIN, 20); 
+        frame.setLayout(null);//using no layout managers  
+        frame.setVisible(false);//making the frame not visible to build pixels  
+        frame.getContentPane().setBackground(Color.BLACK);
+        frame.setSize(pixel_size*pixel_amount_x,pixel_size*pixel_amount_y);
+        for (int x = 0; x < pixel_amount_x; x++) {
+            for (int y = 0; y < pixel_amount_y; y++) {
+                JPanel pixel = new JPanel();
+                pixel.setBounds(x*pixel_size,y*pixel_size,pixel_size,pixel_size);
+                frame.add(pixel);
+            }
+        }
+        frame.setVisible(true);//making the frame visible
     }
 }
