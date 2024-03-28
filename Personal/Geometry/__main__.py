4c01812583e75cd95dff7b9190cf7fa749606951
Benjamin Phillip Hays <67854569+ultradestroyer9123@users.noreply.github.com>
2024-03-28 11:44:30 -0700

    Update __main__.py

--- Personal/Geometry/__main__.py
+++ Personal/Geometry/__main__.py
@@ -3,7 +3,7 @@ import math
 
 
 # Entered Values
-sideAmount = 4
+sideAmount = 50
 sideLength = 30
 
 # Solved Values
@@ -13,19 +13,19 @@ apothem = math.tan(math.radians(angleWanted)) * (sideLength/2)
 radius = math.sqrt((apothem**2) + ((sideLength/2)**2))
 
 # Function to draw a square
-def drawShape():
-    for _ in range(4):
-        turtle.forward(100)  # Adjust the size of the square as needed
-        turtle.right(90)
+def drawShape(sideAmount,sideLength=50):
+    for _ in range(sideAmount):
+        turtle.forward(sideLength)  # Adjust the size of the square as needed
+        turtle.right(360/sideAmount)
 
 # Main function
-def main():
+def main(sideAmount,sideLength):
     turtle.speed(1)  # Set the speed of the turtle (1 is the slowest)
     turtle.penup()   # Lift the pen up
     turtle.goto(-apothem, -apothem)  # Set starting position
     turtle.pendown()  # Put the pen down
-    drawShape()  # Call the function to draw a square
+    drawShape(sideAmount,sideLength)  # Call the function to draw a square
     turtle.done()  # Finish the drawing
 
 if __name__ == "__main__":
-    main()
+    main(sideAmount,sideLength)
